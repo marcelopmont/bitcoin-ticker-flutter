0fecf19bdc23aa3dd4b61aa3c24801c42aaa0bf7
Marcelo Pinheiro Montanher <marcelo.montanher@fcamara.com.br>
2020-04-06 11:12:48 -0300

    Exercise completed

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,16 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future<String> getCoinData(String currencyFrom, String currencyTo) async {
+    var url =
+        'https://rest.coinapi.io/v1/exchangerate/$currencyFrom/$currencyTo?apikey=02D882C3-7FA3-47AA-8AC0-7FEED09DA02C';
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)['rate'].toInt().toString();
+    } else {
+      return '';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:io' show Platform;
+
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
@@ -6,6 +10,74 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  var coinData = CoinData();
+
+  var selectedCurrency = currenciesList[0];
+  var valueFromBitcoin = '?';
+  var valueFromEtherium = '?';
+  var valueFromLTC = '?';
+
+  Widget getPicker() {
+    if (Platform.isIOS) {
+      return getIosPicker();
+    } else {
+      return getAndroidDropdown();
+    }
+  }
+
+  DropdownButton getAndroidDropdown() {
+    List<DropdownMenuItem<String>> list = currenciesList
+        .map((item) => DropdownMenuItem(
+              child: Text(item),
+              value: item,
+            ))
+        .toList();
+
+    return DropdownButton(
+        value: selectedCurrency,
+        items: list,
+        onChanged: (value) async {
+          selectedCurrency = value;
+          onUpdateSelectedCurrency();
+        });
+  }
+
+  CupertinoPicker getIosPicker() {
+    List<Widget> list = currenciesList.map((item) => Text(item)).toList();
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) async {
+        selectedCurrency = currenciesList[selectedIndex];
+        onUpdateSelectedCurrency();
+      },
+      children: list,
+    );
+  }
+
+  onUpdateSelectedCurrency() async {
+    String currencyForBitcoin =
+        await coinData.getCoinData('BTC', selectedCurrency);
+    String currencyForEtherium =
+        await coinData.getCoinData('ETH', selectedCurrency);
+    String currencyForLTC = await coinData.getCoinData('LTC', selectedCurrency);
+
+    setState(() {
+      this.selectedCurrency = selectedCurrency;
+
+      valueFromBitcoin = currencyForBitcoin;
+      valueFromEtherium = currencyForEtherium;
+      valueFromLTC = currencyForLTC;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    onUpdateSelectedCurrency();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,16 +96,45 @@ class _PriceScreenState extends State<PriceScreen> {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      '1 BTC = $valueFromBitcoin $selectedCurrency',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      '1 ETH = $valueFromEtherium $selectedCurrency',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      '1 LTC = $valueFromLTC $selectedCurrency',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -42,7 +143,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: getPicker(),
           ),
         ],
       ),
